8950e8975c798a7aa48d4ada5be36346a8367a64
keshuug <kartikgoswami082@gmail.com>
2022-06-07 19:52:20 +0530

    commit

--- mail.php
+++ mail.php
@@ -8,32 +8,32 @@ if(isset($_POST["btn-send"])){
     // $headers = "From:" . $from;
     // $from = "no-reply@minkucreation.com";
     $to = "connect@minkucreation.com";
-    $messege = """
-    <!DOCTYPE html>
-    <head>
-    <title>Minku Creation</title>
-    <style>
-        body {
-            background: src("/images/uc.png") no-repeat center center fixed black ;
-            -webkit-background-size: cover;
-            -moz-background-size: cover;
-            -o-background-size: cover;
-            background-size: cover;
-        }
-        @media only screen and (max-width: 1200px) {
-            body {
-                background-size: 100%;
-            }
-        }
-    </style>
-    </head>
-    <body>
-    </body>
-    </html>
-    """;
+    // $messege = """
+    // <!DOCTYPE html>
+    // <head>
+    // <title>Minku Creation</title>
+    // <style>
+    //     body {
+    //         background: src("/images/uc.png") no-repeat center center fixed black ;
+    //         -webkit-background-size: cover;
+    //         -moz-background-size: cover;
+    //         -o-background-size: cover;
+    //         background-size: cover;
+    //     }
+    //     @media only screen and (max-width: 1200px) {
+    //         body {
+    //             background-size: 100%;
+    //         }
+    //     }
+    // </style>
+    // </head>
+    // <body>
+    // </body>
+    // </html>
+    // """;
 
     mail($to,$userSub,$userMsg,$userEmail);
-    mail($userEmail,"MinkuCreation",$messege);
+    mail($userEmail,"MinkuCreation","Dear $userName Thanks For Visiting MinkuCreation We Will Contact You Soon On $userEmail");
 
     header("location:index.php?success");
 }else{
