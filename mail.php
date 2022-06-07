06238fa2280bdd4a85a6d55107a5d74a679f4bbe
keshuug <kartikgoswami082@gmail.com>
2022-06-07 19:05:12 +0530

    commit

--- mail.php
+++ mail.php
@@ -5,11 +5,12 @@ if(isset($_POST["btn-send"])){
     $userEmail = $_POST["email"];
     $userSub = $_POST["subject"];
     $userMsg = $_POST["message"];
-
+    $headers = "From:" . $from;
+    $from = "no-reply@minkucreation.com";
     $to = "connect@minkucreation.com";
 
     mail($to,$userSub,$userMsg,$userEmail);
-    mail($userEmail,"MinkuCreation","Thanks For Visiting Minku Creation, We Will Contact You Soon");
+    mail($userEmail,"MinkuCreation","Thanks For Visiting Minku Creation, We Will Contact You Soon",$header);
 
     header("location:index.php?success");
 }else{
