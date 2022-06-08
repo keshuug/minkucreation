4fecf767abc0a797c7cb1c479b47112223f20d9a
keshuug <kartikgoswami082@gmail.com>
2022-06-08 23:32:58 +0530

    commit

--- mail.php
+++ mail.php
@@ -6,26 +6,14 @@ if(isset($_POST["btn-send"])){
     $userSub = $_POST["subject"];
     $userMsg = $_POST["message"];
     // $headers = "From:" . $from;
-    $from = "no-reply@minkucreation.com";
+    $from = "MinkuCreation@minkucreation.com";
     $headers  = "From: $from\r\n"; 
     $headers .= "Content-type: text/html\r\n";
     $to = "connect@minkucreation.com";
-    $messege = `
-    <html>
-    <head>
-    <title>Minku Creation</title>
-    </head>
-    <body>
-    <h1>Minku Creation </h1><br/><br/><br/>
-    <p>Dear $username<br/>
-    Thanks For Visiting <b>Minkucreation</b><br/>
-    We Will Contact You Soon On $userEmail <br/></p>
-    </body>
-    </html>
-    `;
+    $messege = `Hello Minku Here`;
 
     mail($to,$userSub,$userMsg,$userEmail);
-    mail($userEmail,"Minku Creation",$messege,$headers);
+    mail($userEmail,"Thanks For Visiting Minku Creation",$messege,$headers);
 
     header("location:index.php?success");
 }else{
