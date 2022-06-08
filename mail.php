456a54f1b1fb6d0350595eedf855c4452bd1c650
keshuug <kartikgoswami082@gmail.com>
2022-06-08 23:42:30 +0530

    commit

--- mail.php
+++ mail.php
@@ -8,9 +8,23 @@ if(isset($_POST["btn-send"])){
     // $headers = "From:" . $from;
     $from = "MinkuCreation@minkucreation.com";
     $headers  = "From: $from\r\n"; 
-    $headers .= "Content-type: text/html\r\n";
+    $headers[] = 'MIME-Version: 1.0';
+    $headers[] = 'Content-type: text/html; charset=iso-8859-1';
     $to = "connect@minkucreation.com";
-    $messege = "hello minku here";
+    $messege = `
+    <html>
+    <head>
+    <title>Minku Creation</title>
+    </head>
+    <body>
+    <h1>Minku Creation </h1><br/><br/><br/>
+    <p>Dear $username<br/>
+    Thanks For Visiting <b>Minkucreation</b><br/>
+    We Will Contact You Soon On $userEmail <br/></p>
+    </body>
+    </html>
+    `;
+    
 
     mail($to,$userSub,$userMsg,$userEmail);
     mail($userEmail,"Thanks For Visiting Minku Creation",$messege,$headers);
